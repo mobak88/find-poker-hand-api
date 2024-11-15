2c99f96ecd91a240ab23c8964457d6b15deba6fa
mobak88 <mostafa0401@ymail.com>
2024-11-15 15:44:14 +0100

    Correct generated hand

--- src/__tests__/services/deal-cards.test.ts
+++ src/__tests__/services/deal-cards.test.ts
@@ -4,13 +4,13 @@ import { generateRandomNumber } from "../../utils/generate-random-number";
 describe("dealCards", () => {
   it("Should deal five unique cards", () => {
     const testDeck = [
-      { card: "AS", value: 14, suit: "spades" },
-      { card: "2H", value: 2, suit: "hearts" },
-      { card: "3D", value: 3, suit: "diamonds" },
-      { card: "4C", value: 4, suit: "clubs" },
-      { card: "5S", value: 5, suit: "spades" },
-      { card: "6H", value: 6, suit: "hearts" },
-      { card: "7D", value: 7, suit: "diamonds" },
+      { card: "as", value: 14, suit: "spar" },
+      { card: "2h", value: 2, suit: "hjerte" },
+      { card: "3r", value: 3, suit: "ruter" },
+      { card: "4k", value: 4, suit: "kløver" },
+      { card: "5s", value: 5, suit: "spar" },
+      { card: "6h", value: 6, suit: "hjerte" },
+      { card: "7r", value: 7, suit: "ruter" },
     ];
 
     const dealtCards = dealCards(testDeck, generateRandomNumber, 5);
